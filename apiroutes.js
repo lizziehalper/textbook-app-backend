9ad5139054eb7c3a59e6df6730c368aca11cf212
Lizzie Halper <lizzie.halper@gmail.com>
2017-07-21 18:55:20 -0700

    m

--- apiroutes.js
+++ apiroutes.js
@@ -5,9 +5,11 @@ const models = require('./models/models.js');
 // IMPORTING ALL MODELS
 const User = models.User;
 const Book = models.Book;
-const passport = require('passport');
+// const passport = require('passport');
 // hashing
 const crypto = require('crypto');
+
+const isEmail = require('is-email');
 
 function hashPassword(password) {
   const hash = crypto.createHash('sha256');
@@ -16,40 +18,37 @@ function hashPassword(password) {
 }
 
 // ROUTES:
-// router.post('/login', passport.authenticate(('local'), (req, res) => {
-//   const username = req.body.username;
-//   User.findOne({ username }, (err, user) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     // res.json({ success: true,
-//     //   user: {
-//     //     username: user.username,
-//     //     fname: user.fname,
-//     //     lname: user.lname,
-//     //     library: user.library,
-//     //   },
-//     // });
-//     // res.redirect(200, '/');
-//   });
-// }));
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-  res.json({ success: true,
-    user: {
-      id: req.user.id,
-      fname: req.user.fname,
-      lname: req.user.lname,
-      username: req.user.username,
-      email: req.user.email,
-      library: req.user.library,
-    },
-  });
-});
 
 router.post('/register', (req, res) => {
+  const isValidUser = (user) => {
+    if (!user.fname) {
+      return 'Please enter your first name.';
+    }
+    if (!user.lname) {
+      return 'Please enter your last name.';
+    }
+    if (!isEmail(user.email)) {
+      return 'Please enter a valid email address.';
+    }
+    if (!user.username) {
+      return 'Please enter a username.';
+    }
+    if (!user.hashedPassword) {
+      return 'Please enter a password.';
+    }
+    // User.find({ email: user.email })
+    // .exec((err, email) => {
+    //   if (err) {
+    //     return err;
+    //   }
+    //   if (email) {
+    //     return 'There is already an account associated with this email address.';
+    //   }
+    //   return true;
+    // });
+    return true;
+  };
   const newUser = new User({
     fname: req.body.fname,
     lname: req.body.lname,
@@ -58,15 +57,17 @@ router.post('/register', (req, res) => {
     hashedPassword: hashPassword(req.body.password),
     library: [],
   });
-  newUser.save((err) => {
-    if (err) {
-      res.json({ failure: 'failed to save new user' });
-    } else {
-      res.redirect('/login');
-      // res.redirect('/api/registration')
-      console.log('saved the new user!!');
-    }
-  });
+  if (isValidUser(newUser) === true) {
+    newUser.save((err) => {
+      if (err) {
+        res.json({ failure: 'failed to save new user' });
+      } else {
+        res.json({ newUser });
+        // res.redirect('/api/registration')
+        console.log('saved the new user!!');
+      }
+    });
+  }
 });
 
 // gets the user, gets the user's library
--- server.js
+++ server.js
@@ -9,11 +9,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
 // Express set up
 const app = express();
 app.use(bodyParser.json());
 
+
+app.use(bodyParser.urlencoded());
+app.use(expressJWT({ secret: 'secret' }).unless({ path: ['/login', '/register'] }));
+app.use(passport.initialize());
 // Mongoose configuration
 // When there is an error, run this function:
 mongoose.connection.on('error', () => {
@@ -26,19 +31,37 @@ mongoose.connection.on('connected', () => {
 
 mongoose.connect(process.env.MONGODB_URI);
 
+app.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+  const token = jwt.sign({ id: req.body.id }, 'secret');
+  res.json({
+    success: true,
+    user: {
+      id: req.user.id,
+      fname: req.user.fname,
+      lname: req.user.lname,
+      username: req.user.username,
+      email: req.user.email,
+      library: req.user.library,
+    },
+    token,
+  });
+});
+
 // session setup
-const session = require('express-session');
-
-const MongoStore = require('connect-mongo')(session);
-
-app.use(session({
-  secret: 'secret',
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection.on('connected', () => {
-      console.log('NEW SESSION');
-    }),
-  }),
-}));
+// const session = require('express-session');
+//
+// const MongoStore = require('connect-mongo')(session);
+//
+// app.use(session({
+//   secret: 'secret',
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection.on('connected', () => {
+//       console.log('NEW SESSION');
+//     }),
+//   }),
+// }));
 
 // hashing
 const crypto = require('crypto');
@@ -63,33 +86,19 @@ passport.use(new LocalStrategy((username, password, done) => {
   });
 }));
 
-// passport.use(new LocalStrategy((username, password, done) => {
-//   User.findOne({ username }, (err, user) => {
-//     if (err) { return done(err); }
-//     if (!user) {
-//       return done(null, false, { message: 'Incorrect username.' });
-//     }
-//     if (!user.validPassword(password)) {
-//       return done(null, false, { message: 'Incorrect password.' });
-//     }
-//     return done(null, user);
-//   });
-// }));
-
 // PASSPORT SERIALIZE/DESERIALIZE USER HERE HERE
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(null, user);
-  });
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+//
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     done(null, user);
+//   });
+// });
 
 // PASSPORT MIDDLEWARE HERE
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.session());
 
 // require in my routes
 const apiroutes = require('./apiroutes');
